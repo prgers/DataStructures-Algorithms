0a72cf4ca831d1899db0ea6aa452b863aa71f2d1
潘小懒 <prgers@163.com>
2021-06-30 09:01:57 +0800

    添加双向链表代码

--- DataStructure/03-链表/src/com/prger/LinkedList.java
+++ DataStructure/03-链表/src/com/prger/LinkedList.java
@@ -1,16 +1,36 @@
 package com.prger;
 
-import java.util.WeakHashMap;
-
 public class LinkedList<E> extends AbstractList<E> {
     private Node<E> first;
+    private Node<E> last;
 
     private static class Node<E> {
         E element;
         Node<E> next;
-        public Node(E element, Node<E> next) {
+        Node<E> prev;
+        public Node(Node<E> prev, E element, Node<E> next) {
+            this.prev = prev;
             this.element = element;
             this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            if (prev == null) {
+                sb.append("null");
+            }else {
+                sb.append(prev.element);
+            }
+
+            sb.append("_").append(element).append("_");
+            if (next == null) {
+                sb.append("null");
+            }else {
+                sb.append(next.element);
+            }
+
+            return sb.toString();
         }
     }
 
@@ -18,6 +38,7 @@ public class LinkedList<E> extends AbstractList<E> {
     public void clear() {
         size = 0;
         first = null;
+        last = null;
     }
 
     @Override
@@ -37,11 +58,25 @@ public class LinkedList<E> extends AbstractList<E> {
     public void add(int index, E element) {
         rangeCheckForAdd(index);
 
-        if (index == 0) {
-            first = new Node<>(element, first);
+        if (index == size) {
+            Node<E> oldLast = last;
+            last = new Node<>(oldLast, element, null);
+            if (oldLast == null) {
+                first = last;
+            }else {
+                oldLast.next = last;
+            }
         }else {
-            Node<E> prev = node(index -1);
-            prev.next = new Node<>(element, prev.next);
+            Node<E> next = node(index);
+            Node<E> prev = next.prev;
+            Node<E> current = new Node<>(prev, element, next);
+            next.prev = current;
+
+            if (prev == null) {
+                first = current;
+            }else {
+                prev.next = current;
+            }
         }
         size++;
         
@@ -50,13 +85,21 @@ public class LinkedList<E> extends AbstractList<E> {
     @Override
     public E remove(int index) {
         rangeCheck(index);
-        Node<E> node = first;
-        if (index == 0) {
-            first = first.next;
+
+        Node<E> node = node(index);
+        Node<E> prev = node.prev;
+        Node<E> next = node.next;
+
+        if (prev == null) {
+            first = next;
         }else {
-            Node<E> prev = node(index - 1);
-            node = prev.next;
-            prev.next = node.next;
+            prev.next = next;
+        }
+
+        if (next == null) {
+            last = prev;
+        }else {
+            next.prev = prev;
         }
 
         size--;
@@ -86,11 +129,20 @@ public class LinkedList<E> extends AbstractList<E> {
     //获取index位置的节点
     private Node<E> node(int index) {
         rangeCheck(index);
-        Node<E> node = first;
-        for (int i = 0; i < index; i++) {
-            node = node.next;
+
+        if (index < (size >> 1)) {
+            Node<E> node = first;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+            return node;
+        }else {
+            Node<E> node = last;
+            for (int i = size - 1; i < index; i++) {
+                node = node.prev;
+            }
+            return node;
         }
-        return node;
     }
 
     @Override
--- DataStructure/03-链表/src/com/prger/Main.java
+++ DataStructure/03-链表/src/com/prger/Main.java
@@ -3,7 +3,7 @@ package com.prger;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> list = new LinkedList<>();
+        List<Integer> list = new SingleLinkedList<>();
         list.add(20);
         list.add(0, 10);
         list.add(30);
