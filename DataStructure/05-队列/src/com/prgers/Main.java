486679b32e96a0372662538616e7b0a1f4353780
潘小懒 <prgers@163.com>
2021-07-06 06:48:05 +0800

    利用数组实现循环队列

--- DataStructure/05-队列/src/com/prgers/Main.java
+++ DataStructure/05-队列/src/com/prgers/Main.java
@@ -1,5 +1,7 @@
 package com.prgers;
 
+import com.prgers.circle.CircleQueue;
+
 /**
  * @Author prgers
  * @Date 2021/7/5 9:48 下午
@@ -8,7 +10,9 @@ public class Main {
 
     public static void main(String[] args) {
 //        test1();
-        test2();
+//        test2();
+
+        test3();
     }
 
     public static void test1() {
@@ -34,7 +38,28 @@ public class Main {
 
 
         while (!deque.isEmpty()) {
-            System.out.println(deque.deQueueFront());
+            System.out.println(deque.deQueueRear());
         }
+    }
+
+    public static void test3() {
+        CircleQueue<Integer> queue = new CircleQueue<>();
+
+        // 0 1 2 3 4 5 6 7 8 9
+        for (int i = 0; i < 10; i++) {
+            queue.enQueue(i);
+        }
+        // null null null null null 5 6 7 8 9
+        for (int i = 0; i < 5; i++) {
+            queue.deQueue();
+        }
+        // 15 16 17 18 19 5 6 7 8 9
+        for (int i = 15; i < 20; i++) {
+            queue.enQueue(i);
+        }
+//        while (!queue.isEmpty()) {
+//            System.out.println(queue.deQueue());
+//        }
+        System.out.println(queue);
     }
 }
