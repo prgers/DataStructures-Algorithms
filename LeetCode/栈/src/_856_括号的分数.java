8d8ff5eb16b0c729a59df45ff5dd8f9e4dbbb149
潘小懒 <prgers@163.com>
2021-07-06 08:40:23 +0800

    提交代码

--- LeetCode/栈/src/_856_括号的分数.java
+++ LeetCode/栈/src/_856_括号的分数.java
@@ -6,8 +6,18 @@ import java.util.Stack;
  * https://leetcode-cn.com/problems/score-of-parentheses/
  */
 public class _856_括号的分数 {
-//    public int scoreOfParentheses(String s) {
-//        Stack<Character> stack = new Stack<>();
-//
-//    }
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(0);
+            }else {
+                int x = stack.pop();
+                int y = stack.pop();
+                stack.push(y + Math.max(2 * x, 1));
+            }
+        }
+        return stack.pop();
+    }
 }
