988dae02918538c1a9cfd0b4c0594741605b043d
潘小懒 <prgers@163.com>
2021-07-11 20:46:34 +0800

    提交代码

---
+++ LeetCode/二叉树/src/com/prgers/TreeNode.java
@@ -0,0 +1,18 @@
+package com.prgers;
+
+/**
+ * @Author prgers
+ * @Date 2021/7/11 1:28 下午
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ LeetCode/二叉树/src/com/prgers/_226_翻转二叉树.java
@@ -0,0 +1,21 @@
+package com.prgers;
+
+/**
+ * @Author prgers
+ * @Date 2021/7/11 1:27 下午
+ * https://leetcode-cn.com/problems/invert-binary-tree/
+ */
+public class _226_翻转二叉树 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
